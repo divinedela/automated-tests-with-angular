dad23b846150d958c2c1418e8e05d3c27ea905e6
divine dela <divinedela.dd@gmail.com>
2019-07-07 21:03:46 +0000

    add set up for test
    Set ups are used to do initialisations that need to happen before each
    test runs. If needed tear downs can be used to do clean ups after test
    runs.

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,20 @@
 import { VoteComponent } from './vote.component'; 
 
 describe('VoteComponent', () => {
-  it('', () => {
+  let component: VoteComponent;
+  beforeEach(() => {
+    component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should increment total votes when upvoted', () => {
+    component.upVote();
+
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('should decrement total votes when downvoted', () => {
+    component.downVote();
+
+    expect(component.totalVotes).toBe(-1);
   });
 });
