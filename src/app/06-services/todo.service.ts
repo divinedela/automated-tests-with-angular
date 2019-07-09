5df2838aea1a0aba69d16fee60c1710fc257923a
divine dela <divinedela.dd@gmail.com>
2019-07-09 10:57:50 +0000

    working with spies
    Spies help us get control over a method in a class - We can check if
    the method has been called, return a different value, change the
    implementation of the method or throw an error

--- src/app/06-services/todo.service.ts
+++ src/app/06-services/todo.service.ts
@@ -1,6 +1,5 @@
 
 import { HttpClient } from '@angular/common/http';
-import 'rxjs/add/operator/map';
 
 export class TodoService {
   constructor(private http: HttpClient) {
--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,24 @@
 import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
+import { of } from 'rxjs';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
+  let service: TodoService;
 
   beforeEach(() => {
+    service = new TodoService(null);
+    component = new TodosComponent(service);
   });
 
-  it('', () => {
+  it('should set todos property with the items received from the server', () => {
+    const todos = [1, 2, 3];
+    spyOn(service, 'getTodos').and.callFake(() => {
+      return of(todos);
+    });
+
+    component.ngOnInit();
+
+    expect(component.todos.length).toBe(3);
   });
 });
